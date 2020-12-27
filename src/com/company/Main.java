83dde32bfe13d82469445f278b6e43e050388252
XIAODONG HUO <39690442+hxiaodong0@users.noreply.github.com>
2020-12-27 16:37:32 -0500

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++){
+
+            if (nums[i] != nums[j]){
+                i++;
+            }
+            nums[i] = nums[j];
+        }
+
+        return i+1;
+
+
+    }
+
+    public static void main(String[] args) {
+//        List<Integer> nums = new ArrayList<Integer>(Arrays.asList(1,1,2));
+        int[] nums = new int[3];
+        nums[0] = 1;
+        nums[1] = 1;
+        nums[2] = 2;
+        int[] test = {1,1,2,2,2,2,2,3,4,4,5};
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(test));
+        int TF = removeDuplicates(test);
+        System.out.println(Arrays.toString(test));
+        System.out.println(TF);
+    }
+}
